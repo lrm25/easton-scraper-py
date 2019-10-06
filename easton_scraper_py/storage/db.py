561d03ec4599a6d025049d70b5e7ea3a7b8f1dd5
Luke McCrone <luke.r.mccrone@gmail.com>
2019-10-06 12:19:40 -0600

    Patch data save issue (db folder creation)

--- easton_scraper_py/storage/db.py
+++ easton_scraper_py/storage/db.py
@@ -22,10 +22,23 @@ IDX_DESCRIPTION_LINK = 11
 
 #
 # write easton class data to text file, so it can be retrieved without querying internet if desired
+# returns True if successfully written, False if not
 #
 def write(easton_class):
 
-    f = open("storage/db/{}_{}.db".format(easton_class.get_gym_db_name(), easton_class.get_id()), "w")
+    if not os.path.exists("storage/db"):
+        try:
+            os.mkdir("storage/db")
+        except OSError as e:
+            print("Fatal error creating folder:  {}".format(e))
+            return False
+
+    try:
+        filename = "storage/db/{}_{}.db".format(easton_class.get_gym_db_name(), easton_class.get_id())
+        f = open(filename, "w")
+    except OSError as e:
+        print("Error writing file {} to disk".format(e))
+        return False
 
     f.write("{}{}".format(DB_VERSION, os.linesep))
     f.write("{}{}".format(easton_class.get_gym_name(), os.linesep))
@@ -41,6 +54,7 @@ def write(easton_class):
     f.write("{}{}".format(easton_class.get_description_link(), os.linesep))
 
     f.close()
+    return True
 
 
 #
@@ -70,34 +84,35 @@ def delete_classes_done_before_now():
 def load(class_day_list):
 
     easton_classes = []
-    for db_file in os.listdir("storage/db"):
-        db_file_split = db_file.split('_')
-        if len(db_file_split) == 1:
-            print("Warning:  invalid filename in db folder, no underscore: {}".format(db_file))
-            continue
+    if os.path.exists("storage/db"):
+        for db_file in os.listdir("storage/db"):
+            db_file_split = db_file.split('_')
+            if len(db_file_split) == 1:
+                print("Warning:  invalid filename in db folder, no underscore: {}".format(db_file))
+                continue
 
-        gym_db_name = db_file_split[0]
-        with open("storage/db/{}".format(db_file), "r") as f:
-            lines = f.readlines()
-            if lines[IDX_DB_VERSION].strip() == str(DB_VERSION):
-                if lines[IDX_DATE].strip() not in class_day_list:
-                    continue
+            gym_db_name = db_file_split[0]
+            with open("storage/db/{}".format(db_file), "r") as f:
+                lines = f.readlines()
+                if lines[IDX_DB_VERSION].strip() == str(DB_VERSION):
+                    if lines[IDX_DATE].strip() not in class_day_list:
+                        continue
 
-                easton_class = EastonClass(lines[IDX_GYM_NAME].strip(),
-                                           gym_db_name,
-                                           lines[IDX_CLASS_ID].strip(),
-                                           lines[IDX_NAME].strip(),
-                                           lines[IDX_DATE].strip(),
-                                           lines[IDX_START_TIME].strip(),
-                                           lines[IDX_END_TIME].strip())
-                easton_class.set_instructor(lines[IDX_INSTRUCTOR].strip())
-                easton_class.set_sortable_start_time(datetime.strptime(lines[IDX_SORTABLE_START_TIME].strip(),
-                                                                       "%Y-%m-%d %H:%M:%S"))
-                easton_class.set_sortable_end_time(datetime.strptime(lines[IDX_SORTABLE_END_TIME].strip(),
-                                                                     "%Y-%m-%d %H:%M:%S"))
-                easton_class.set_cancelled(ast.literal_eval(lines[IDX_CANCELLED]))
-                easton_class.set_description_link(lines[IDX_DESCRIPTION_LINK])
-                easton_classes.append(easton_class)
+                    easton_class = EastonClass(lines[IDX_GYM_NAME].strip(),
+                                               gym_db_name,
+                                               lines[IDX_CLASS_ID].strip(),
+                                               lines[IDX_NAME].strip(),
+                                               lines[IDX_DATE].strip(),
+                                               lines[IDX_START_TIME].strip(),
+                                               lines[IDX_END_TIME].strip())
+                    easton_class.set_instructor(lines[IDX_INSTRUCTOR].strip())
+                    easton_class.set_sortable_start_time(datetime.strptime(lines[IDX_SORTABLE_START_TIME].strip(),
+                                                                           "%Y-%m-%d %H:%M:%S"))
+                    easton_class.set_sortable_end_time(datetime.strptime(lines[IDX_SORTABLE_END_TIME].strip(),
+                                                                         "%Y-%m-%d %H:%M:%S"))
+                    easton_class.set_cancelled(ast.literal_eval(lines[IDX_CANCELLED]))
+                    easton_class.set_description_link(lines[IDX_DESCRIPTION_LINK])
+                    easton_classes.append(easton_class)
     return easton_classes
 
 
